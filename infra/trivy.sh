96f2a56b3afcba1e93d3180b9952dea58d744313
DINESH MEKA <mekadinesh28@gmail.com>
2024-12-24 16:08:17 +0530

    Update trivy.sh

--- infra/trivy.sh
+++ infra/trivy.sh
@@ -1,6 +1,8 @@
 curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sudo sh -s -- -b /usr/local/bin
 trivy --version
-echo $PATH
-echo 'export PATH=$PATH:/usr/local/bin' >> ~/.bashrc
-source ~/.bashrc
-trivy --version
+
+
+# execute externally
+# echo $PATH
+# echo 'export PATH=$PATH:/usr/local/bin' >> ~/.bashrc
+# source ~/.bashrc
